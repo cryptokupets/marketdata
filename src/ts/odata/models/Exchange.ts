556bc0c584c03169c44fc159259dcc22ca14e3d6
Dmitry Fokin <fokind1@yandex.ru>
2019-06-07 20:12:10 +0300

    добавлена функция

--- src/ts/odata/models/Exchange.ts
+++ src/ts/odata/models/Exchange.ts
@@ -1,9 +1,20 @@
-import { Edm } from "odata-v4-server";
+import { Edm, odata } from "odata-v4-server";
 
 export class Exchange {
   @Edm.Key
   @Edm.String
   public key: string
+
+  @Edm.Function
+  @Edm.String
+  public getCandles(
+    @Edm.String currency: string,
+    @Edm.String asset: string,
+    @Edm.String period: string,
+    @odata.result result: any
+  ): any {
+    return { currency, asset, period, exchange: result.key };
+  }
 
   constructor({ key }: { key: string }) {
     Object.assign(this, { key });
