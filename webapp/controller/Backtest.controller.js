35e8d5a5e94908200fa4cf8cb27584832c72d3a9
Dmitry Fokin <fokind1@yandex.ru>
2019-08-16 21:21:13 +0300

    run with log results

--- webapp/controller/Backtest.controller.js
+++ webapp/controller/Backtest.controller.js
@@ -1,150 +1,194 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/Item"], function(
-  Controller,
-  Item
-) {
-  "use strict";
+/* global moment */
 
-  return Controller.extend("ck.marketdata.controller.Backtest", {
-    onInit: function() {
-      this.getOwnerComponent()
-        .getRouter()
-        .getRoute("backtest")
-        .attachMatched(this._onRouteMatched, this);
-      this.getView().addStyleClass(
-        this.getOwnerComponent().getContentDensityClass()
-      );
-    },
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Item",
+    "../thirdparty/moment-with-locales"
+  ],
+  function(Controller, Item) {
+    "use strict";
 
-    _onRouteMatched: function(oEvent) {
-      var oController = this;
-      var oView = this.getView();
-      var oModel = oView.getModel();
-      var oDraftModel = oView.getModel("draft");
-      var sBacktestId = oEvent.getParameter("arguments").id;
+    return Controller.extend("ck.marketdata.controller.Backtest", {
+      onInit: function() {
+        this.getOwnerComponent()
+          .getRouter()
+          .getRoute("backtest")
+          .attachMatched(this._onRouteMatched, this);
+        this.getView().addStyleClass(
+          this.getOwnerComponent().getContentDensityClass()
+        );
+      },
 
-      oView.bindElement({
-        path: "/Backtest('" + sBacktestId + "')",
-        parameters: {
-          $expand: "Exchange($expand=Currencies,Timeframes)"
-        },
-        events: {
-          dataReceived: function() {
-            oController._bindAssets();
-            var oBindingContext = oView.getBindingContext();
+      _onRouteMatched: function(oEvent) {
+        var oController = this;
+        var oView = this.getView();
+        var oDraftModel = oView.getModel("draft");
+        var sBacktestId = oEvent.getParameter("arguments").id;
 
-            var sStart = oBindingContext.getProperty("start").slice(0, 10);
-            oDraftModel.setProperty("/start", sStart);
-            var sEnd = oBindingContext.getProperty("end").slice(0, 10);
-            oDraftModel.setProperty("/end", sEnd);
-            var sTimeframe = oBindingContext.getProperty("timeframe");
-            oView
-              .getModel("view")
-              .setProperty(
+        oView.bindElement({
+          path: "/Backtest('" + sBacktestId + "')",
+          parameters: {
+            $expand: "Exchange($expand=Currencies,Timeframes)"
+          },
+          events: {
+            dataReceived: function() {
+              var oBindingContext = oView.getBindingContext();
+              oDraftModel.setProperty(
+                "/currencyKey",
+                oBindingContext.getProperty("currencyKey")
+              );
+              oController._bindAssets();
+              oDraftModel.setProperty(
+                "/assetKey",
+                oBindingContext.getProperty("assetKey")
+              );
+              oDraftModel.setProperty(
                 "/timeframe",
-                moment
-                  .duration(
-                    +sTimeframe.slice(1),
-                    sTimeframe.slice(0, 1).toLowerCase()
-                  )
-                  .asMinutes()
+                oBindingContext.getProperty("timeframe")
+              );
+              oDraftModel.setProperty(
+                "/strategyIndicators",
+                oBindingContext.getProperty("strategyIndicators")
+              );
+              oDraftModel.setProperty(
+                "/strategyParameters",
+                oBindingContext.getProperty("strategyParameters")
+              );
+              oDraftModel.setProperty(
+                "/stoplossEnabled",
+                oBindingContext.getProperty("stoplossEnabled")
+              );
+              oDraftModel.setProperty(
+                "/stopLossLevel",
+                oBindingContext.getProperty("stopLossLevel")
+              );
+              oDraftModel.setProperty(
+                "/fee",
+                oBindingContext.getProperty("fee")
+              );
+              oDraftModel.setProperty(
+                "/strategyCode",
+                oBindingContext.getProperty("strategyCode")
               );
 
-            oController._draw();
+              var sStart = oBindingContext.getProperty("start").slice(0, 10);
+              oDraftModel.setProperty("/start", sStart);
+              var sEnd = oBindingContext.getProperty("end").slice(0, 10);
+              oDraftModel.setProperty("/end", sEnd);
+              var sTimeframe = oBindingContext.getProperty("timeframe");
+              oView
+                .getModel("view")
+                .setProperty(
+                  "/timeframe",
+                  moment
+                    .duration(
+                      +sTimeframe.slice(1),
+                      sTimeframe.slice(0, 1).toLowerCase()
+                    )
+                    .asMinutes()
+                );
+            }
           }
-        }
-      });
-    },
-
-    _bindAssets: function() {
-      var oBindingContext = this.getView().getBindingContext();
-      var sCurrencyKey = oBindingContext.getProperty("currencyKey");
-      var sExchangeKey = oBindingContext.getProperty("exchangeKey");
-      this.byId("asset").bindAggregation("items", {
-        path:
-          "/Currency(key='" +
-          sCurrencyKey +
-          "',exchangeKey='" +
-          sExchangeKey +
-          "')/Assets",
-        length: 10000,
-        template: new Item({
-          text: "{key}",
-          key: "{key}"
-        }),
-        templateShareable: false
-      });
-    },
-
-    onCurrencyChange: function() {
-      this._bindAssets();
-    },
-
-    _draw: function() {
-      var oView = this.getView();
-      var oClose = this.byId("close");
-      var oBalance = this.byId("balance");
-      var oBindingContext = oView.getBindingContext();
-      var sBacktestId = oBindingContext.getProperty("_id");
-
-      oView
-        .getModel("buffer")
-        .loadData("/odata/Backtest('" + sBacktestId + "')/Output")
-        .finally(function() {
-          oClose.refresh();
-          oBalance.refresh();
         });
-    },
+      },
 
-    onRefresh: function() {
-      this._draw();
-    },
+      _bindAssets: function() {
+        var oView = this.getView();
+        var oBindingContext = oView.getBindingContext();
+        var sExchangeKey = oBindingContext.getProperty("exchangeKey");
+        var sCurrencyKey = oView.getModel("draft").getProperty("/currencyKey");
 
-    onBackPress: function() {
-      this.getOwnerComponent()
-        .getRouter()
-        .navTo("backtests");
-    },
+        this.byId("asset").bindAggregation("items", {
+          path:
+            "/Currency(key='" +
+            sCurrencyKey +
+            "',exchangeKey='" +
+            sExchangeKey +
+            "')/Assets",
+          length: 10000,
+          template: new Item({
+            text: "{key}",
+            key: "{key}"
+          }),
+          templateShareable: false
+        });
+      },
 
-    onStartChange: function() {
-      var oView = this.getView();
-      oView
-        .getBindingContext()
-        .setProperty(
-          "start",
-          moment
-            .utc(oView.getModel("draft").getProperty("/start"))
-            .toISOString()
-        );
-      // TODO после сохранения обновить Output
-    },
+      onCurrencyChange: function() {
+        this._bindAssets();
+      },
 
-    onEndChange: function() {
-      var oView = this.getView();
-      oView.getBindingContext().setProperty(
-        "end",
-        moment
-          .utc(oView.getModel("draft").getProperty("/end"))
-          .add(1, "d")
-          .add(-1, "s")
-          .toISOString()
-      );
-    },
+      onBackPress: function() {
+        this.getOwnerComponent()
+          .getRouter()
+          .navTo("backtests");
+      },
 
-    onTimeframeChange: function() {
-      var oView = this.getView();
-      var sTimeframe = oView.getBindingContext().getProperty("timeframe");
-      // var iTimeframeMilliseconds = moment.duration(+sTimeframe.slice(1), sTimeframe.slice(0, 1).toLowerCase()).asMilliseconds();
-      oView
-        .getModel("view")
-        .setProperty(
-          "/timeframe",
-          moment
-            .duration(
-              +sTimeframe.slice(1),
-              sTimeframe.slice(0, 1).toLowerCase()
-            )
-            .asMinutes()
-        );
-    }
-  });
-});
+      _draftToJSON: function() {
+        var oDraftModel = this.getView().getModel("draft");
+        return JSON.stringify({
+          exchangeKey: "hitbtc",
+          assetKey: oDraftModel.getProperty("/assetKey"),
+          currencyKey: oDraftModel.getProperty("/currencyKey"),
+          end: moment
+            .utc(oDraftModel.getProperty("/end"))
+            .add(1, "d")
+            .add(-1, "s")
+            .toISOString(),
+          fee: oDraftModel.getProperty("/fee"),
+          initialBalance: 1,
+          start: moment.utc(oDraftModel.getProperty("/start")).toISOString(),
+          stopLossLevel: oDraftModel.getProperty("/stopLossLevel"),
+          stoplossEnabled: oDraftModel.getProperty("/stoplossEnabled"),
+          strategyCode: oDraftModel.getProperty("/strategyCode"),
+          strategyIndicators: oDraftModel.getProperty("/strategyIndicators"),
+          strategyParameters: oDraftModel.getProperty("/strategyParameters"),
+          timeframe: oDraftModel.getProperty("/timeframe")
+        });
+      },
+
+      // _save: function() {
+      //   return $.ajax({
+      //     async: true,
+      //     url:
+      //       "/odata" +
+      //       this.getView()
+      //         .getBindingContext()
+      //         .getPath(),
+      //     method: "PATCH",
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //     data: this._draftToJSON()
+      //   });
+      // },
+
+      onRunPress: function() {
+        var oRouter = this.getOwnerComponent().getRouter();
+        var sBacktestId;
+        // создать новый
+        $.ajax({
+          async: true,
+          url: "/odata/Backtest",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: this._draftToJSON()
+        })
+          .then(function(oData) {
+            // выполнить
+            sBacktestId = oData._id;
+            return $.post("/odata/Backtest('" + sBacktestId + "')/odata.run");
+          })
+          .then(function() {
+            // перейти на новую страницу
+            oRouter.navTo("backtest", {
+              id: sBacktestId
+            });
+          });
+      }
+    });
+  }
+);
